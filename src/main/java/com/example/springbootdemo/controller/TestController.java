c1bb64743693c2a552864b446ebe14c2cac5c676
yc <1677585879@qq.com>
2026-05-30 22:02:23 +0800

    初始化项目

---
+++ src/main/java/com/example/springbootdemo/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "恭喜您，成功了";
+    }
+}
